7298e128161c750f2960f98f79127af2325f686a
Daniel Janusch <djanusch2007@gmail.com>
2026-05-26 00:14:23 -0700

    add tools/crc32-gen.py
    It generates VHDL code which can be redirected to `crc32_60.vhd` or whatever else.
    The generator works for any data size, but it is only arbitrary-length at generation time.

---
+++ tools/crc32-gen.py
@@ -0,0 +1,77 @@
+from zlib import crc32
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data-len", type=int, default=60, help="bytes length of checksum input data. default is 60")
+parser.add_argument("--in-port",  type=str, default="data", help="VHDL input port name. default is 'data'")
+parser.add_argument("--out-port", type=str, default="crc", help="VHDL output port name. default is 'crc'")
+args = parser.parse_args()
+
+data_len = args.data_len
+sum_len  = 4 # 32 bits
+in_port  = args.in_port
+out_port = args.out_port
+
+cols = [crc32((1 << n).to_bytes(data_len, byteorder='big')) for n in range(8*data_len)]
+
+rows = [
+	[8*data_len - 1 - n for n in range(8*data_len) if cols[n] & (1 << bit)]
+	for bit in range(8*sum_len)
+]
+
+K = crc32(bytes(data_len))
+
+max_pad = 1 + max(len(in_port), len(out_port))
+in_pad  = " "*(max_pad - len(in_port))
+out_pad = " "*(max_pad - len(out_port))
+
+print(f"""\
+-- Generated with tools/crc32-gen.py
+library ieee;
+use ieee.std_logic_1164;
+
+entity crc32_{data_len} is
+\tport (
+\t\t{in_port}{in_pad}: in  std_logic_vector({8*data_len - 1} downto 0);
+\t\t{out_port}{out_pad}: out std_logic_vector({8*sum_len - 1} downto 0);
+\t);
+end entity;
+
+architecture crc32_{data_len}_arch of crc32_{data_len} is
+	-- polynomial: 0x04C11DB7
+	-- CRC32( 0 ): 0x04128908
+begin\
+""")
+
+for bit in range(8*sum_len):
+	terms = " xor ".join(f"{in_port}({n})" for n in rows[bit])
+	print(f"\t{out_port}({bit:2d}) <= '{(K >> bit) & 1}' xor {terms};")
+
+print("end architecture;")
+
+
+"""
+## test code
+
+data = bytes([
+	0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE,
+	0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD,
+	0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC,
+	0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB,
+])
+
+inp = []
+for byte in data:
+	for bit in range(7, -1, -1):
+		inp.append((byte >> bit) & 1)
+
+
+crc = [0] * 32
+# this is not the best way to do this, but I don't really care
+for bit in range(8*sum_len):
+	terms = " ^ ".join(f"data[{n}]" for n in rows[bit])
+	exec(f"crc[{bit}] = {(K >> bit) & 1} ^ {terms}")
+
+print(f"expected = {crc32(data):032b}")
+print(f"actual   = {"".join(str(x) for x in reversed(crc))}")
+"""
